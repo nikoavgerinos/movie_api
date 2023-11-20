2e5fb04cb3f5211a3ffb7d2b3d2cb5a0db0a0cff
nikoavgerinos <avgerinos.niko@gmail.com>
2023-11-20 13:33:31 +0100

    feat: Add initial project files and basic server setup (Task 2.2)
    This commit marks the initial setup of the "movie-api" project, incorporating key files and basic server functionality.
    
    Changes Made
    Created a "documentation.html" file within the "movie_api" directory, providing initial instructions for developers on how to use the API. This document will be continuously updated as the project progresses.
    Added a "server.js" file to the project directory, importing the "http" module, and configured it to listen for requests on port 8080.
    Imported the "url" module at the top of the "server.js" file for further request handling.
    Implemented request URL parsing to determine if the URL contains the keyword "documentation." If true, the server returns the "documentation.html" file; otherwise, it returns the "index.html" file.
    Created a "log.txt" file in the project directory to log incoming requests along with timestamps using the "fs" module.
    Project Directory
    The "movie-api" directory now includes the following files:
    
    "test.js"
    "index.html"
    "documentation.html"
    "server.js" (imports "http," "fs," and "url" modules)
    "log.txt"

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ http.createServer((request, response) => {
 
         response.writeHead(200, { 'Content-Type': 'text/plain' });
         response.write(data);
-        response.end();
+        response.end('hello node');
     });
 
 }).listen(8080);
 
-console.log('My first Node test server is running on Port 8080.');
+console.log('Node working');
