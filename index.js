f2414d47efa93b8b468ba08b39c0e0402e956b55
nikoavgerinos <avgerinos.niko@gmail.com>
2023-11-23 11:57:21 +0100

    feat(api): Implement REST API endpoints and documentation (Task 2.5)
    ### Changes Made
    1. Created REST API endpoints in `index.js` to address the following tasks:
        - Return a list of ALL movies to the user.
        - Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user.
        - Return data about a genre (description) by name/title (e.g., “Coach Carter”).
        - Return data about a director (bio, birth year, death year) by name.
        - Allow new users to register.
        - Allow users to update their user info (username).
        - Allow users to add a movie to their list of favorites (showing only a text that a movie has been added).
        - Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed).
        - Allow existing users to deregister (showing only a text that a user email has been removed).
    
    2. Added documentation in `public/documentation.html`.
       - Implemented bootstrap to build a reactive table.
       - Created documentation for the project for every endpoint.

--- index.js
+++ index.js
@@ -1,137 +1,270 @@
-const express = require('express'),
-morgan = require('morgan'),
-fs = require('fs'), // import built in node modules fs and path 
-path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser'); // Add this line
+const uuid = require('uuid');
+
 const app = express();
 
+app.use(bodyParser.json()); // Add this line to parse JSON bodies
+const accessLogStream = fs.createWriteStream(path.join('log.txt'), { flags: 'a' });
+// setup the logger
+app.use(morgan('common'));
 
 
 
-const movieList = [
+// top 10 movies
+let topMovies = [
     {
         title: 'Coach Carter',
-        genre: 'Drama, Sports',
+        description: 'Coach Carter is a rieviting sports drama that follows the inspiring true story of Coach Ken Carter, who shocks the basketball community by benching his undefeated team to prioritize academics, teaching them life lessons that transcend the court and ingite a powerful journey of self-discovery and triumph.',
+        genre: 'Drama',
         actors: 'Samuel L. Jackson, Rick Gonzales, Robert Richard',
         director: 'Thomas Carter',
         releaseYear: '2005',
         rating: 'PG-13',
-        length: '136 mins'
+        length: '136 mins',
+        imageUrl: 'https://www.themoviedb.org/t/p/original/y3HOTTyM5nLsdUzXFtFCohG28qj.jpg'
 
     },
     {
         title: 'Movie 2',
-        genre: '',
-        actors: '',
-        director: 'Director 2',
-        releaseYear: '',
-        rating: '',
-        length: ''
-        
+
+        director: 'Director 2'
+
+
     },
     {
         title: 'Movie 3',
-        genre: '',
-        actors: '',
-        director: 'Director 3',
-        releaseYear: '',
-        rating: '',
-        length: ''
+
+        director: 'Director 3'
+
     },
     {
         title: 'Movie 4',
-        genre: '',
-        actors: '',
+
         director: 'Director 4',
-        releaseYear: '',
-        rating: '',
-        length: ''
+
     },
     {
         title: 'Movie 5',
-        genre: '',
-        actors: '',
+
         director: 'Director 5',
-        releaseYear: '',
-        rating: '',
-        length: ''
+
     },
     {
         title: 'Movie 6',
-        genre: '',
-        actors: '',
+
         director: 'Director 6',
-        releaseYear: '',
-        rating: '',
-        length: ''
+
     },
     {
         title: 'Movie 7',
-        genre: '',
-        actors: '',
+
         director: 'Director 7',
-        releaseYear: '',
-        rating: '',
-        length: ''
+
     },
     {
         title: 'Movie 8',
-        genre: '',
-        actors: '',
         director: 'Director 8',
-        releaseYear: '',
-        rating: '',
-        length: ''
+
     },
     {
         title: 'Movie 9',
-        genre: '',
-        actors: '',
+
         director: 'Director 9',
-        releaseYear: '',
-        rating: '',
-        length: ''
+
     },
     {
-        title: 'Movie 10',
-        genre: '',
-        actors: '',
-        director: 'Director 10',
-        releaseYear: '',
-        rating: '',
-        length: ''
+        Title: 'Movie 10',
+        director: 'Director 10'
+    }
+];
+
+//users
+let users = [
+    {
+        id: 1,
+        name: "George",
+        email: "george.miller@gmail.com",
+        password: "george123",
+        favoriteMovies: ["Coach Carter"],
+        
+    },
+    {
+        id: 2,
+        name: "Bryant",
+        email: "bryant.smith@gmail.com",
+        password: "bryant123",
+        favoriteMovies: []
+    }
+];
+
+let movies = [
+    {
+        "title": "Coach Carter",
+        "year": "2005",
+        "image": "https://www.themoviedb.org/t/p/original/y3HOTTyM5nLsdUzXFtFCohG28qj.jpg",
+        "description": "Coach Carter is a riveting sports drama that follows the inspiring true story of Coach Ken Carter, who shocks the basketball community by benching his undefeated team to prioritize academics, teaching them life lessons that transcend the court and ignite a powerful journey of self-discovery and triumph.",
+        "director": [
+            {
+                "Name": "Thomas Carter",
+                "Birth": "1953",
+                "Death": "",
+                "Bio": "Thomas Carter is an accomplished American film and television director, known for his work on 'Coach Carter' and other notable projects. With a career spanning decades, he has made significant contributions to the entertainment industry."
+            }
+        ],
+        "genres": [  
+            {
+                "genreName": "Drama",
+                "genreDescription": "Drama is a genre of fiction characterized by discourses or works intended to evoke intense emotions, often focusing on the profound and serious aspects of human experience, compelling audiences through compelling narratives and emotional resonance."
+            },
+        ]
     },
 ];
 
-// logging setup
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' })
-app.use(morgan('combined', { stream: accessLogStream }));
 
-//app routing setup
-app.use(express.static('public'));
 
-// GET request
-app.get('/', (req, res) =>{
-    res.send('Welcome to myFlix application!');
+// GET requests
+app.get('/', (req, res) => {
+    res.send('Welcome to my top 10 movies!');
 });
-app.get('/documentation.html', (req, res) =>{
-    res.sendFile('public/documentation.html', { root: __dirname });
+
+// CREATE - allow users to register
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+
+    if (!newUser || !newUser.name) {
+        res.status(400).json({ error: 'Bad Request', message: 'Please provide a valid user object with a name.' });
+        return;
+    }
+
+    newUser.id = uuid.v4();
+    users.push(newUser);
+
+    res.status(201).json(newUser);
 });
+
+
+//UPDATE - Allow users to update their user info (username)
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.name = updatedUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send('no such user');
+    }
+});
+
+//CREATE - Allow users to add a movie to their list of favorites 
+app.post('/users/:id/:movietitle', (req, res) => {
+    const { id, movietitle } = req.params;
+
+    if (!movietitle) {
+        res.status(400).send('Please provide a movie title');
+        return;
+    }
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies.push(movietitle);
+        res.status(200).json(`${movietitle} has been added to user ${id}'s favorite list`);
+    } else {
+        res.status(400).send('no such user')
+    }
+});
+
+//DELETE - Allow users to remove a movie from their list of favorites
+app.delete('/users/:id/:movietitle', (req, res) => {
+    const { id, movietitle } = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies = user.favoriteMovies.filter(title => title !== movietitle);
+        res.status(200).json(`${movietitle} has been removed from user ${id}'s favorite list`);
+    } else {
+        res.status(400).send('no such user')
+    }
+});
+
+//DELETE - Allow existing users to delete their account (de-register)
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        users = users.filter(user => user.id != id);
+        res.status(200).json(`user ${id} has been deleted`);
+    } else {
+        res.status(400).send('no such user')
+    }
+});
+
 app.get('/movies', (req, res) => {
-    res.json(movieList);
+    res.json(movies);
+    //READ - return a list of all movies 
+    app.get('/movies', (req, res) => {
+        res.status(200).json(movies);
+    });
+
+});
+//READ - return data about a single movie by name
+app.get('/movies/:title', (req, res) => {
+    const requestedTitle = req.params.title;
+    const movie = movies.find(movie => movie.title.toLowerCase() === requestedTitle.toLowerCase());
+
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(404).send('Movie not found :(');
+    }
 });
 
-// error handling
+// READ - return data about a genre by name
+app.get('/movies/genre/:genreName', (req, res) => {
+    const { genreName } = req.params;
+    const movie = movies.find(movie =>
+        movie.genres.some(genre => genre.genreName.toLowerCase() === genreName.toLowerCase())
+    );
+
+    if (movie) {
+        const genre = movie.genres.find(genre => genre.genreName.toLowerCase() === genreName.toLowerCase());
+        const genreInfo = {
+            genreName: genre.genreName,
+            genreDescription: genre.genreDescription,
+        };
+        res.status(200).json(genreInfo);
+    } else {
+        res.status(404).send('Genre not found :(');
+    }
+});
+//READ - return data about a director by name 
+app.get('/movies/directors/:directorName', (req, res) => {
+    const { directorName } = req.params;
+    const director = movies.find(movie => movie.director[0].Name === directorName);
+
+    if (director) {
+        res.status(200).json(director.director[0]);
+    } else {
+        res.status(404).send('Director not found :(');
+    }
+});
+
+// Create error-handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Something broke!');
-
-})
-
-
-
-
-
-
-app.listen(8080, () => {
-    console.log('The movie app has loaded and is listening on port 8080');
+    res.status(500).json({ error: 'Internal Server Error', message: err.message });
 });
+// listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});  
