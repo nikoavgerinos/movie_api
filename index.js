a97ea754107fbdf40794372a919352972bd1a2b0
nikoavgerinos <avgerinos.niko@gmail.com>
2023-11-28 16:51:49 +0100

    feat(mongoDB): Implement MongoDB integration and update documentation (Task 2.5)
    ### Changes Made
    
    1. Added `model.js` to implement MongoDB integration.
    2. Updated `index.js` to create MongoDB endpoints for the following tasks:
        - Return a list of ALL movies.
        - Return data about a single movie by title.
        - Return data about a genre by name/title.
        - Return data about a director by name.
        - Allow new users to register.
        - Allow users to update their user info (username).
        - Allow users to add a movie to their list of favorites.
        - Allow users to remove a movie from their list of favorites.
        - Allow existing users to deregister.
    
    3. Updated documentation in `documentation.html`.
       - Implemented Bootstrap for a reactive table.
       - Created comprehensive documentation for each endpoint.

--- index.js
+++ index.js
@@ -2,269 +2,251 @@ const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
-const bodyParser = require('body-parser'); // Add this line
+const bodyParser = require('body-parser');
 const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie; // Use the correct property name 'Movie'
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 const app = express();
 
-app.use(bodyParser.json()); // Add this line to parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const accessLogStream = fs.createWriteStream(path.join('log.txt'), { flags: 'a' });
-// setup the logger
+
 app.use(morgan('common'));
 
+// Return a list of ALL movies to the user 
+app.get('/movies', async (req, res) => {
+    try {
+        const allMovies = await Movies.find(); // Retrieve all movies from the 'Movies' collection
 
+        // Create a new array with the desired order of fields
+        const formattedMovies = allMovies.map(movie => ({
+            _id: movie._id,
+            Title: movie.Title,
+            Description: movie.Description,
+            Genre: movie.Genre,
+            Director: movie.Director,
+            ImagePath: movie.ImagePath,
+            Featured: movie.Featured
+        }));
 
-// top 10 movies
-let topMovies = [
-    {
-        title: 'Coach Carter',
-        description: 'Coach Carter is a rieviting sports drama that follows the inspiring true story of Coach Ken Carter, who shocks the basketball community by benching his undefeated team to prioritize academics, teaching them life lessons that transcend the court and ingite a powerful journey of self-discovery and triumph.',
-        genre: 'Drama',
-        actors: 'Samuel L. Jackson, Rick Gonzales, Robert Richard',
-        director: 'Thomas Carter',
-        releaseYear: '2005',
-        rating: 'PG-13',
-        length: '136 mins',
-        imageUrl: 'https://www.themoviedb.org/t/p/original/y3HOTTyM5nLsdUzXFtFCohG28qj.jpg'
-
-    },
-    {
-        title: 'Movie 2',
-
-        director: 'Director 2'
-
-
-    },
-    {
-        title: 'Movie 3',
-
-        director: 'Director 3'
-
-    },
-    {
-        title: 'Movie 4',
-
-        director: 'Director 4',
-
-    },
-    {
-        title: 'Movie 5',
-
-        director: 'Director 5',
-
-    },
-    {
-        title: 'Movie 6',
-
-        director: 'Director 6',
-
-    },
-    {
-        title: 'Movie 7',
-
-        director: 'Director 7',
-
-    },
-    {
-        title: 'Movie 8',
-        director: 'Director 8',
-
-    },
-    {
-        title: 'Movie 9',
-
-        director: 'Director 9',
-
-    },
-    {
-        Title: 'Movie 10',
-        director: 'Director 10'
+        res.status(200).json(formattedMovies);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
     }
-];
+});
+// Return data about a single movie by title to the user
+app.get('/movies/:title', async (req, res) => {
+    try {
+        const movie = await Movies.findOne({ Title: req.params.title });
 
-//users
-let users = [
-    {
-        id: 1,
-        name: "George",
-        email: "george.miller@gmail.com",
-        password: "george123",
-        favoriteMovies: ["Coach Carter"],
-        
-    },
-    {
-        id: 2,
-        name: "Bryant",
-        email: "bryant.smith@gmail.com",
-        password: "bryant123",
-        favoriteMovies: []
+        if (!movie) {
+            return res.status(404).json({ error: 'Movie not found' });
+        }
+
+        // Extract specific fields you want to return to the user
+        const { Title, Description, Genre, Director, ImageURL, Featured } = movie;
+
+        res.status(200).json({
+            Title,
+            Description,
+            Genre,
+            Director,
+            ImageURL,
+            Featured,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
     }
-];
-
-let movies = [
-    {
-        "title": "Coach Carter",
-        "year": "2005",
-        "image": "https://www.themoviedb.org/t/p/original/y3HOTTyM5nLsdUzXFtFCohG28qj.jpg",
-        "description": "Coach Carter is a riveting sports drama that follows the inspiring true story of Coach Ken Carter, who shocks the basketball community by benching his undefeated team to prioritize academics, teaching them life lessons that transcend the court and ignite a powerful journey of self-discovery and triumph.",
-        "director": [
-            {
-                "Name": "Thomas Carter",
-                "Birth": "1953",
-                "Death": "",
-                "Bio": "Thomas Carter is an accomplished American film and television director, known for his work on 'Coach Carter' and other notable projects. With a career spanning decades, he has made significant contributions to the entertainment industry."
-            }
-        ],
-        "genres": [  
-            {
-                "genreName": "Drama",
-                "genreDescription": "Drama is a genre of fiction characterized by discourses or works intended to evoke intense emotions, often focusing on the profound and serious aspects of human experience, compelling audiences through compelling narratives and emotional resonance."
-            },
-        ]
-    },
-];
-
-
-
-// GET requests
-app.get('/', (req, res) => {
-    res.send('Welcome to my top 10 movies!');
 });
 
-// CREATE - allow users to register
-app.post('/users', (req, res) => {
-    const newUser = req.body;
+// Return data about a genre (description) by name/title
+app.get('/genres/:name', async (req, res) => {
+    const genreName = req.params.name;
 
-    if (!newUser || !newUser.name) {
-        res.status(400).json({ error: 'Bad Request', message: 'Please provide a valid username.' });
-        return;
+    try {
+        // Find the first movie with the specified genre name
+        const movieWithGenre = await Movies.findOne({ 'Genre.Name': genreName });
+
+        if (movieWithGenre) {
+            const genreDescription = movieWithGenre.Genre.Description;
+            res.status(200).json({ Name: genreName, Description: genreDescription });
+        } else {
+            res.status(404).send('Genre not found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
     }
-
-    newUser.id = uuid.v4();
-    users.push(newUser);
-
-    res.status(201).json(newUser);
 });
 
+// Return data about a director (bio, birth year, death year) by name
+app.get('/directors/:name', async (req, res) => {
+    const directorName = req.params.name;
 
-//UPDATE - Allow users to update their user info (username)
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
+    try {
+        // Find the first movie with the specified director name
+        const movieWithDirector = await Movies.findOne({ 'Director.Name': directorName });
 
-    let user = users.find(user => user.id == id);
+        if (movieWithDirector) {
+            const directorInfo = {
+                Name: directorName,
+                Bio: movieWithDirector.Director.Bio,
+                Birth: movieWithDirector.Director.Birth,
+                Death: movieWithDirector.Director.Death,
+            };
+            res.status(200).json(directorInfo);
+        } else {
+            res.status(404).send('Director not found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
 
-    if (user) {
-        user.name = updatedUser.name;
+// User registration 
+app.post('/users', async (req, res) => {
+    try {
+        const newUser = await Users.create(req.body);
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+// Update user endpoint
+app.put('/users/:username', async (req, res) => {
+    const username = req.params.username;
+
+    try {
+        const updatedUser = await Users.findOneAndUpdate(
+            { Username: username },
+            { $set: req.body },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+// Add a movie to a user's list of favorites
+app.post('/users/:username/favorites/:movieId', async (req, res) => {
+    const username = req.params.username;
+    const movieId = req.params.movieId;
+
+    try {
+        // Check if the user exists
+        const user = await Users.findOne({ Username: username });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Check if the movie exists
+        const movie = await Movies.findById(movieId);
+
+        if (!movie) {
+            return res.status(404).json({ error: 'Movie not found' });
+        }
+
+        // Check if the movie is already in the user's favorites
+        if (user.FavoriteMovies.includes(movieId)) {
+            return res.status(400).json({ error: 'Movie already in favorites' });
+        }
+
+        // Add the movie to the user's favorites
+        user.FavoriteMovies.push(movieId);
+        await user.save();
+
+        res.status(201).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+// Remove a movie from the user's list of favorites
+app.delete('/users/:username/favorites/:movieId', async (req, res) => {
+    const username = req.params.username;
+    const movieId = req.params.movieId;
+
+    try {
+        // Find the user by username
+        const user = await Users.findOne({ Username: username });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Check if the movieId is in the user's favorites
+        const movieIndex = user.FavoriteMovies.indexOf(movieId);
+
+        if (movieIndex === -1) {
+            return res.status(404).json({ error: 'Movie not found in favorites' });
+        }
+
+        // Remove movie from users favorites (using MovieID)
+        user.FavoriteMovies.splice(movieIndex, 1);
+
+        // Save the updated user
+        await user.save();
+
         res.status(200).json(user);
-    } else {
-        res.status(400).send('user not found :(');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
     }
 });
 
-//CREATE - Allow users to add a movie to their list of favorites 
-app.post('/users/:id/:movietitle', (req, res) => {
-    const { id, movietitle } = req.params;
+// Deregister user 
+app.delete('/users/:username', async (req, res) => {
+    const username = req.params.username;
 
-    if (!movietitle) {
-        res.status(400).send('movie not found :(');
-        return;
-    }
+    try {
+        // Check if the user exists
+        const existingUser = await Users.findOne({ Username: username });
 
-    let user = users.find(user => user.id == id);
+        if (!existingUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
 
-    if (user) {
-        user.favoriteMovies.push(movietitle);
-        res.status(200).json(`${movietitle} has been added to user ${id}'s favorite list`);
-    } else {
-        res.status(400).send('user not found :(')
-    }
-});
+        // Remove the user from the database
+        await Users.deleteOne({ Username: username });
 
-//DELETE - Allow users to remove a movie from their list of favorites
-app.delete('/users/:id/:movietitle', (req, res) => {
-    const { id, movietitle } = req.params;
-
-    let user = users.find(user => user.id == id);
-
-    if (user) {
-        user.favoriteMovies = user.favoriteMovies.filter(title => title !== movietitle);
-        res.status(200).json(`${movietitle} has been removed from user ${id}'s favorite list`);
-    } else {
-        res.status(400).send('no such user')
+        res.status(200).json({ message: 'User deregistered successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
     }
 });
 
-//DELETE - Allow existing users to delete their account (de-register)
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
-
-    let user = users.find(user => user.id == id);
-
-    if (user) {
-        users = users.filter(user => user.id != id);
-        res.status(200).json(`user ${id} has been deleted`);
-    } else {
-        res.status(400).send('user not found :(')
-    }
-});
-
-app.get('/movies', (req, res) => {
-    res.json(movies);
-    //READ - return a list of all movies 
-    app.get('/movies', (req, res) => {
-        res.status(200).json(movies);
-    });
-
-});
-//READ - return data about a single movie by name
-app.get('/movies/:title', (req, res) => {
-    const requestedTitle = req.params.title;
-    const movie = movies.find(movie => movie.title.toLowerCase() === requestedTitle.toLowerCase());
-
-    if (movie) {
-        res.status(200).json(movie);
-    } else {
-        res.status(404).send('Movie not found :(');
-    }
-});
-
-// READ - return data about a genre by name
-app.get('/movies/genre/:genreName', (req, res) => {
-    const { genreName } = req.params;
-    const movie = movies.find(movie =>
-        movie.genres.some(genre => genre.genreName.toLowerCase() === genreName.toLowerCase())
-    );
-
-    if (movie) {
-        const genre = movie.genres.find(genre => genre.genreName.toLowerCase() === genreName.toLowerCase());
-        const genreInfo = {
-            genreName: genre.genreName,
-            genreDescription: genre.genreDescription,
-        };
-        res.status(200).json(genreInfo);
-    } else {
-        res.status(404).send('Genre not found :(');
-    }
-});
-//READ - return data about a director by name 
-app.get('/movies/directors/:directorName', (req, res) => {
-    const { directorName } = req.params;
-    const director = movies.find(movie => movie.director[0].Name === directorName);
-
-    if (director) {
-        res.status(200).json(director.director[0]);
-    } else {
-        res.status(404).send('Director not found :(');
-    }
-});
-
-// Create error-handling
+// Create error-handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ error: 'Internal Server Error', message: err.message });
 });
+
 // listen for requests
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
-});  
+});
